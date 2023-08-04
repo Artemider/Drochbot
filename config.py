c13193153b7a275d84233b8b53a4290ab3de1edc
Artemider <99029606+Artemider@users.noreply.github.com>
2023-08-04 11:50:40 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,15 +1,15 @@
-TOKEN = '5210691709:AAHToWAUVZuJzsl6mrl-bQCQaLTQfXWwBa4'
+TOKEN = ''
 
 #TOKEN = '5431759596:AAGK8h3J6p7a8RF-vueVg-lefOCL2xGsJUg'
 
 base = 'basedata.db'
 
-owner = ["1101984099"]
+owner = [""]
 
-admins = ["1101984099"]
+admins = [""]
 
-moderator = ["1101984099"]
+moderator = [""]
 
-VIP = ["1101984099", "1667849940", "1832789743"]
+VIP = ["", "", ""]
 
 ignore = []
